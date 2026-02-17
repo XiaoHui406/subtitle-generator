3e643757a374b62f1f14ca6ccf0c8595f86dec5b
XiaoHui406 <1052120423@qq.com>
2026-02-17 23:49:15 +0800

    refactor: 将main.py中的两个接口迁移至asr_router.py refactor: load_asr_model接口重命名为load_model refactor: run_asr接口重命名为transcribe

--- backend/main.py
+++ backend/main.py
@@ -5,31 +5,11 @@ import os
 from typing import List
 from registry import asr_model_manager
 from model.transcription_segment import TranscriptionSegment
+from router.asr_router import asr_router
 
 app = FastAPI()
 
-
-@app.get("/load_asr_model/{model_name}")
-def load_asr_model(model_name: str) -> str:
-    asr_model_manager.load_model()
-    return "load asr model successed"
-
-
-@app.post('/run_asr/{output_format}')
-def run_asr(file: UploadFile, output_format: str) -> List[TranscriptionSegment]:
-    temp_file = f'audio/temp_{file.filename}'
-    with open(temp_file, 'wb') as buffer:
-        shutil.copyfileobj(file.file, buffer)
-
-    result: List[TranscriptionSegment] = asr_model_manager.transcribe(
-        audio=temp_file,
-        output_format=output_format
-    )
-
-    if os.path.exists(temp_file):
-        os.remove(temp_file)
-
-    return result
+app.include_router(asr_router)
 
 
 if __name__ == "__main__":
--- backend/router/asr_router.py
+++ backend/router/asr_router.py
@@ -1,6 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
+from typing import List
+import os
+import shutil
+from model.transcription_segment import TranscriptionSegment
+from registry import asr_model_manager
+
 
 asr_router = APIRouter(
     prefix='/asr',
     tags=['asr']
 )
+
+
+@asr_router.get("/load_asr_model")
+def load_model(model_name: str, model_size: str) -> str:
+    asr_model_manager.load_model()
+    return "load asr model successed"
+
+
+@asr_router.post('/transcribe/{output_format}')
+def transcribe(file: UploadFile, output_format: str) -> List[TranscriptionSegment]:
+    temp_file = f'audio/temp_{file.filename}'
+    with open(temp_file, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    result: List[TranscriptionSegment] = asr_model_manager.transcribe(
+        audio=temp_file,
+        output_format=output_format
+    )
+
+    if os.path.exists(temp_file):
+        os.remove(temp_file)
+
+    return result
