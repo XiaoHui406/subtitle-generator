4f58d8ad685f494ba35d8fe8010af13d6c7b52a9
XiaoHui406 <1052120423@qq.com>
2026-02-20 00:08:35 +0800

    feat: 用户访问“/”接口时会重定向到“/docs”接口

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 import uvicorn
-from registry import asr_model_manager
 from router.asr_router import asr_router
 
 app = FastAPI()
 
 app.include_router(asr_router)
+
+
+@app.get('/')
+def to_docs():
+    return RedirectResponse(url='/docs')
 
 
 if __name__ == "__main__":
