3bae3df99440a1a09afe77f7453dfd484cc38c60
XiaoHui406 <1052120423@qq.com>
2026-02-19 22:11:39 +0800

    feat: 编写FunasrModelManager中transcribe一部分代码

--- backend/manager/asr/funasr_model_manager.py
+++ backend/manager/asr/funasr_model_manager.py
@@ -1,7 +1,7 @@
 from manager.asr.interface.asr_model_manager import ASRModelManager
 from model.transcription_segment import TranscriptionSegment
 from torch.cuda import is_available
-from typing import List
+from typing import List, Dict
 from funasr import AutoModel
 
 
@@ -15,13 +15,38 @@ class FunasrModelManager(ASRModelManager):
         super().__init__()
         self.model: AutoModel | None = None
         self.model_size: str = model_size
+        self.vad_model: AutoModel | None = None
         self.device: str = 'cuda' if not device and is_available() else 'cpu'
 
     def load_model(self) -> None:
-        ...
+        self.model = AutoModel(
+            model="iic/SenseVoiceSmall",
+            trust_remote_code=True,
+            device="cuda:0"
+        )
+        self.vad_model = AutoModel(model='fsmn-vad')
 
     def unload_model(self) -> None:
-        ...
+        del self.model
+        self.model: AutoModel | None = None
 
     def transcribe(self, audio: str) -> List[TranscriptionSegment]:
-        ...
+        if not self.model or not self.vad_model:
+            self.load_model()
+        if self.model and self.vad_model:
+            vad_result = self.vad_model.generate(
+                input=audio
+            )
+            assert type(vad_result) is Dict
+            # vad_result 输出示例
+    #       [
+    #           {
+    #             "key": "speech",
+    #             "value": [
+    #                 [
+    #                     3880,
+    #                     11090
+    #                 ],
+            segments = vad_result[0]['value']
+            for (start, end) in segments:
+                assert (type(start), type(end)) is (int, int)
